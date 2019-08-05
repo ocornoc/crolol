0e6442a19dbe4bd8aa77e317d117bff206ab43d6
Grayson Burton <ocornoc@protonmail.com>
2019-08-04 17:51:30 -0700

    Reimplemented exponents
    Now, at least integer powers work, both with integers and non-integers.

--- src/losslessops.cpp
+++ src/losslessops.cpp
@@ -1,7 +1,9 @@
+#include <algorithm>
+#include <array>
 #include <cstdint>
 #include <cstdlib>
 #include <limits>
-#include <stdexcept>
+#include <numeric>
 #include <boost/multiprecision/cpp_int.hpp>
 #include "losslessops.h"
 
@@ -57,31 +59,59 @@ saferet backend::divide(int64_t n, int64_t m)
 	else return clamp((static_cast<int128>(n) * scale) / static_cast<int128>(m));
 }
 
+using approx_table_type = std::array<int64_t, scale>;
+
+static int64_t approx_table_func(int64_t n)
+{
+	return static_cast<int64_t>(mp::sqrt(static_cast<int128>(n) * scale));
+}
+
+static approx_table_type approx_table()
+{
+	approx_table_type atab;
+	std::iota(atab.begin(), atab.end(), 0);
+	std::transform(atab.begin(), atab.end(), atab.begin(),
+		approx_table_func);
+	
+	return atab;
+}
+
+static const approx_table_type sqrt_approx = approx_table();
+
+// Assumes input is positive.
+static int64_t sqrt(int64_t n)
+{
+	if (n >= scale) return 2*sqrt(n / 4);
+	else return sqrt_approx.at(n);
+}
+
 saferet backend::pow(int64_t n, int64_t m)
 {
-	if (n < 0 and std::abs(m) % scale != 0) return make_badarg(m);
+	if (n < 0 and m % scale != 0) return make_badarg(m);
 	else if (n == 0 and m <= 0) return make_badarg(m);
-	else if (m == 0) return clamp(scale);
-	else if (n == 0) return clamp(0);
+	else if (n == scale or m == 0) return clamp(scale);
+	else if (m == scale) return clamp(n);
 	else if (m < 0) {
-		const saferet mul = divide(-scale, m);
-		if (mul.flow != noflow) return mul;
-		const saferet p = pow(n, mul.val);
-		switch (p.flow) {
-			case badarg: return p;
-			case noflow: return divide(scale, p.val);
-			default: return clamp(0);
-		}
-	} else if (m == scale or n == scale) return clamp(n);
-	else if (m < scale) {
-		const int128 rescaled_n = static_cast<int128>(n) * scale;
-		const int64_t sqrtn = static_cast<int64_t>(mp::sqrt(
-			static_cast<int128>(rescaled_n)));
+		const saferet result = pow(n, -m);
+		if (result.flow == overflow or result.flow == underflow)
+			return clamp(0);
+		else if (result.flow == badarg) return result;
 		
-		return pow(sqrtn, 2 * m);
+		return divide(scale, result.val);
+	} else if (m > scale) {
+		int64_t accum = scale;
+		
+		for (int64_t i = 0; i < m; i += scale) {
+			saferet result = multiply(n, accum);
+			if (result.flow != noflow) return result;
+			accum = result.val;
+		}
+		
+		const saferet remain = pow(n, m % scale);
+		if (remain.flow != noflow) return remain;
+		
+		return multiply(remain.val, accum);
 	} else {
-		const saferet p = pow(n, m - scale);
-		if (p.flow == noflow) return multiply(n, p.val);
-		else return p;
+		return pow(sqrt(n), 2 * m);
 	}
 }
