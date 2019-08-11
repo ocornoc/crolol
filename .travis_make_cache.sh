01c27bd878a1d3164544219efab2055e651de621
Grayson Burton <ocornoc@protonmail.com>
2019-08-10 18:03:25 -0700

    Attempted cache support
    Caches the boost compiled (?) components, then installs them in
    `before_script`.
    
    Added new install bash file
    
    Fixed syntactic typo in YAML

---
+++ .travis_make_cache.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+wget https://dl.bintray.com/boostorg/release/1.70.0/source/${BOOSTNAME}.zip
+unzip -q ${BOOSTNAME}.zip
+cd ${BOOSTNAME}
+sudo ./bootstrap.sh
+sudo ./b2 stage -j 2 -d0 --with-math
+cd ..
